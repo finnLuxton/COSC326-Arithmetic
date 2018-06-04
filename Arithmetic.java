c138bb0692f076ae8e03f4d5213bb1d590a669c5
Finlay Luxton <luxfi448@student.otago.ac.nz>
2018-06-05 11:35:46 +1200

    Bug Fixes ~ - Takes in multiple lines of input from a file - Bug where incorrect sums were achieved for a * calculation

--- Arithmetic.java
+++ Arithmetic.java
@@ -5,6 +5,7 @@
 import java.util.Scanner;
 import java.lang.Character;
 import java.lang.StringBuilder;
+import java.util.Arrays;
 
 public class Arithmetic{
 
@@ -14,48 +15,56 @@ public class Arithmetic{
     public static void main(String[] args){
 
         String inputNumbers;
+        String[] stringSplitter = null;
+        int[] numbers = null;
         char operation;
-        int target, i;
+        String targetOp;
+        int i, target;
         int length = 0;
+
         Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()){
+            
+            inputNumbers = scanner.nextLine();
+            // Takes in user input and makes it useable
+            targetOp = scanner.nextLine();
+            operation = targetOp.charAt(targetOp.length() - 1);
+            targetOp = targetOp.substring(0, targetOp.length() - 2);
+            operation = Character.toUpperCase(operation);
+            target = Integer.parseInt(targetOp);
 
-        // Takes in user input and makes it useable
-        System.out.println("Please enter the numbers to be used");
-        inputNumbers = scanner.nextLine();
-        System.out.println("Please enter the target number followed by N or L");
-        target = scanner.nextInt();
-        operation = scanner.next().charAt(0);
-        operation = Character.toUpperCase(operation);
+            // Turns the input numbers into a string, and removes all occurances of spaces
+            stringSplitter = null;
+            stringSplitter = inputNumbers.split(" ");  
+            length = stringSplitter.length;
 
-        // Turns the input numbers into a string, and removes all occurances of spaces
-        String[] stringSplitter = inputNumbers.split(" ");  
-        length = stringSplitter.length;
+            // Initializes a new int array with a size that can hold the inputted numbers
+            numbers = null;
+            numbers = new int[length];
 
-        // Initializes a new int array with a size that can hold the inputted numbers
-        int[] numbers = new int[length];
+            // Fill the int array with contents from the string array
+            for(i = 0; i < length; i++){
+                numbers[i] = Integer.parseInt(stringSplitter[i]);
+            }
 
-        // Fill the int array with contents from the string array
-        for(i = 0; i < length; i++){
-            numbers[i] = Integer.parseInt(stringSplitter[i]);
+            // If the user specified L operation, run that method. Otherwise, print error
+            if(operation == 'L'){
+                lrOperation(numbers, target, 0, 0);
+            }else if(operation == 'N'){ 
+                System.out.println("You printed N!");
+            }else{
+               System.err.println("ERROR: Please enter L or N for operation only");
+            }
+            
+            // If the method adds anything to operand, it found a correct answer. The program prints that answer
+            // Otherwise, it prints impossible.
+            if(operandBuilder.length() > 0){
+                System.out.println(operation + " " + (new StringBuilder(operandBuilder).reverse().toString()));
+            }else{
+                System.out.println(operation + " " + target + " impossible");
+            }
+            operandBuilder = "";
         }
-
-        // If the user specified L operation, run that method. Otherwise, print error
-        if(operation == 'L'){
-            lrOperation(numbers, target, 0, 0);
-        }else if(operation == 'N'){ 
-            System.out.println("You printed N!");
-        }else{
-            System.err.println("ERROR: Please enter L or N for operation only");
-        }
-        
-        // If the method adds anything to operand, it found a correct answer. The program prints that answer
-        // Otherwise, it prints impossible.
-        if(operandBuilder.length() > 0){
-            System.out.println(operation + " " + (new StringBuilder(operandBuilder).reverse().toString()));
-        }else{
-            System.out.println(operation + " " + target + " impossible");
-        }
-        
         // Close the scanner, effectively ending the program
         scanner.close();
 
@@ -79,10 +88,12 @@ public class Arithmetic{
 
         // Checks to see if the current calculation is over the target
         if(!((sum * numbers[depth]) > target) || !((sum + numbers[depth]) > target)){
+                       
             // Checks to see if this if this current number is the last in inputs
             if(depth == numbers.length - 1){
                 // If any current calculations are on target, return true and begin building the operandString
                 if(sum + numbers[depth] == target){
+
                     isPossible = true;
                     if(length == 1){
                         operandBuilder += numbers[depth];
@@ -93,6 +104,10 @@ public class Arithmetic{
                     return true;
                 }
                 if(sum * numbers[depth] == target){
+                    System.out.println("Sum: " + sum);
+                    System.out.println("Curr: " + numbers[depth]);
+                    int temp = sum * numbers[depth];
+                    System.out.println("Sum * Curr: " + temp);
                     isPossible = true;
                     if(length == 1){
                         operandBuilder += numbers[depth];
@@ -101,8 +116,8 @@ public class Arithmetic{
                     }
                     operandBuilder += " * ";
                     return true;
-                }
-            }else{
+                }   
+            }else{            
                 // Checks to see if the next calculation is over the target or not. If under, keep recurring.
                 if(!(sum + numbers[depth] > target)){
                     if (lrOperation(numbers, target, sum + numbers[depth], depth + 1)){
@@ -118,11 +133,13 @@ public class Arithmetic{
                             }
                         }
                         return true;
-                    }
-                    
+                    }                
                 }
                 // Checks to see if the next calculation is over the target or not. If under, keep recurring.
                 if(!(sum * numbers[depth] > target)){
+                    if(depth == 0){
+                        sum = 1;
+                    }
                     if (lrOperation(numbers, target, sum * numbers[depth], depth + 1)){
                         // Flag will be thrown if calculation found in previous recursion. Begin building string recursively
                         if(isPossible){
@@ -137,7 +154,7 @@ public class Arithmetic{
                         }
                         return true;
                     }
-                }
+                }              
             }
         }
         return false;
